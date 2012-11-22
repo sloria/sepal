c0d8f18570341c8aaeb9ed4d00120cd0d86b43b9
Steven Loria <sloria1@gmail.com>
2012-11-22 09:51:23 -0600

    Add Label model

--- sqk/datasets/tests.py
+++ sqk/datasets/tests.py
@@ -2,10 +2,6 @@ from django.test import TestCase
 from tasks import read_datasource
 from models import *
 
-class SimpleTest(TestCase):
-    def test_simple(self):
-        self.assertEqual(1+1,2)
-
 class ReadDataSourceTest(TestCase):
     def setUp(self):
         self.d = Dataset.objects.create(name='iris-data')
