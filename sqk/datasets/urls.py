fb7e03369869775c235941f156e10082fd00dbf9
Steven Loria <sloria1@gmail.com>
2012-11-23 02:03:15 -0600

    Moved class-based views to views.py

--- sqk/datasets/urls.py
+++ sqk/datasets/urls.py
@@ -1,27 +1,14 @@
 '''urlconf for sqk.datasets'''
 
 from django.conf.urls.defaults import url, patterns
-from django.views.generic import ListView, DetailView, CreateView
-from sqk.datasets.forms import DatasetForm
-from sqk.datasets import views
-from sqk.datasets.models import Dataset
-from sqk.datasets.views import DatasetCreate
+from sqk.datasets.views import DatasetList, DatasetDetail, DatasetCreate
 
 urlpatterns = patterns('',
     # ex: /datasets/
-    url(r'^$', ListView.as_view(
-            queryset=Dataset.objects.all(),
-            context_object_name='all_datasets',
-            template_name='datasets/index.html'
-            ), 
-        name='index'),
+    url(r'^$', DatasetList.as_view(), name='index'),
 
     # ex: /datasets/3/
-    url(r'^(?P<pk>\d+)/$', DetailView.as_view(
-            model=Dataset,
-            context_object_name='dataset',
-            template_name='datasets/detail.html'
-            ),
+    url(r'^(?P<pk>\d+)/$', DatasetDetail.as_view(),
         name='detail'),
 
     # ex: /datasets/new/
--- sqk/datasets/views.py
+++ sqk/datasets/views.py
@@ -1,10 +1,26 @@
-from django.views.generic import CreateView
+from django.views.generic import ListView, DetailView, CreateView
 from sqk.datasets.forms import DatasetForm
 from sqk.datasets.models import Dataset
+
+class DatasetList(ListView):
+    model = Dataset
+    queryset = Dataset.objects.all()
+    context_object_name = 'all_datasets'
+    template_name='datasets/index.html'
+
+class DatasetDetail(DetailView):
+    model = Dataset
+    context_object_name = 'dataset'
+    template_name='datasets/detail.html'
 
 class DatasetCreate(CreateView):
     model = Dataset
     form_class = DatasetForm
     template_name='datasets/create.html'
     success_url='/datasets/' #TODO: remove this and dfine get_absolute_url()
+
+    # TODO: 
+    # def form_valid(self, form):
+        # add read_dataset to task queue
+
 
