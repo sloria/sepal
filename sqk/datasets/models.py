f264a06db669df1017df60d932b301dac7208233
Steven Loria <sloria1@gmail.com>
2012-11-23 10:32:34 -0600

    Add is_label_name field to Feature model

--- sqk/datasets/models.py
+++ sqk/datasets/models.py
@@ -30,6 +30,7 @@ class Feature(models.Model):
         null=True, 
         related_name='features')
     name = models.CharField(max_length=100, unique=True)
+    is_label_name = models.BooleanField(default=False)
     def __unicode__(self):
         return self.name
 
