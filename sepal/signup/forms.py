a3b3a6591f161d43a6249062ece41a258510c654
Peter Collins <pmcollins2@wisc.edu>
2013-02-11 23:03:54 -0600

    Fix so email should work with password

--- sepal/signup/forms.py
+++ sepal/signup/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from django.core.mail import send_mail
 
 class SignupForm(forms.Form):
     email = forms.EmailField()
--- sepal/signup/views.py
+++ sepal/signup/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.views.decorators.csrf import csrf_protect
+from django.core.mail import send_mail
 
 from sepal.signup.forms import SignupForm
+from sepal.settings import EMAIL_HOST_USER 
 
 @csrf_protect
 def signup(request):
@@ -12,5 +14,5 @@ def signup(request):
             user = User.objects.create_user(form.cleaned_data['email'], form.cleaned_data['email'], '')
             user.is_active = False;
             user.save()
-            send_mail('New Signup Invite Request by ' + user.email, 'A new user requested beta signup for sepal. Go here to set them a password and then send them an email.\n http://sepalbio.com/admin/auth/user/'+user.id+'/', EMAIL_HOST_USER, [EMAIL_HOST_USER], fail_silently=False)
+            send_mail('New Signup Invite Request by '+user.email, 'A new user requested beta signup for sepal. Go here to set them a password and then send them an email.\n http://sepalbio.com/admin/auth/user/'+str(user.id)+'/', EMAIL_HOST_USER, [EMAIL_HOST_USER], fail_silently=False)
     return HttpResponseRedirect('/')
