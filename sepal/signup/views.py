b63ad276b542e8613a0928b42a69081cbac8c0e8
Steven Loria <sloria1@gmail.com>
2013-02-28 00:02:24 -0600

    Can't request more than one invite from same email

--- sepal/signup/views.py
+++ sepal/signup/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.views.decorators.csrf import csrf_protect
 from sepal.signup.tasks import send_request_email
@@ -10,10 +11,25 @@ def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
-            # Create an inactive new user
-            user = User.objects.create_user(form.cleaned_data['email'], form.cleaned_data['email'], '')
-            user.is_active = False
-            user.save()
-            # Send an email to notify admin of the new request
-            send_request_email.delay(user)
+            if not user_exists(form.cleaned_data['email']):
+                # Create an inactive new user
+                user = User.objects.create_user(form.cleaned_data['email'], 
+                                                form.cleaned_data['email'], '')
+                user.is_active = False
+                user.save()
+                # Send an email to notify admin of the new request
+                send_request_email.delay(user)
+                messages.success(request, 'Your invite request was sent. \
+                Thank you for your interest in Sepal!')
+            else:
+                # Don't allow multiple requests from the same email
+                messages.info(request, 'An account already exists or an \
+                    invite request has already been submitted from this e-mail. \
+                    If you are receiving this message in error, please contact \
+                    sepalproject@gmail.com.')
     return HttpResponseRedirect('/')
+
+def user_exists(username):
+    if User.objects.filter(username=username).count():
+        return True
+    return False
